c5b7b83dc61f5591687be44c2f3765d44b7fec25
Frances Webb <frances.webb@cornell.edu>
2018-04-10 12:10:37 -0400

    Update start time
    This will not remain necessary for long!!!

--- src/main/java/edu/cornell/library/integration/availability/MonitorAvailability.java
+++ src/main/java/edu/cornell/library/integration/availability/MonitorAvailability.java
@@ -22,7 +22,7 @@ import edu.cornell.library.integration.voyager.Items;
 public class MonitorAvailability {
 
   public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, XMLStreamException, SolrServerException {
-    Timestamp since = Timestamp.valueOf("2018-04-06 09:40:00.0");
+    Timestamp since = Timestamp.valueOf("2018-04-10 11:50:00.0");
 
     Properties prop = new Properties();
     try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties")){
