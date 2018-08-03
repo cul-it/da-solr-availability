0a0654a36c0c01ce25d256bd4fe867adcd770ed4
Frances Webb <frances.webb@cornell.edu>
2018-08-03 17:52:49 -0400

    Update call number filter to recognize configurable prefixes
    DISCOVERYACCESS-19
    
    This will make it easy to supply the call numbers with prefixes to the index, as well as accept searches with prefixes from patrons, all while sorting according to the call number sans prefex. The side effect is that the prefixes are not, themselves, browseable, but they will still be searchable in the general call number search.

--- src/main/java/edu/cornell/library/integration/solr/CallNumberSortFilter.java
+++ src/main/java/edu/cornell/library/integration/solr/CallNumberSortFilter.java
@@ -3,6 +3,8 @@ package edu.cornell.library.integration.solr;
 import java.io.IOException;
 //import java.lang.invoke.MethodHandles;
 import java.text.Normalizer;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
@@ -19,9 +21,11 @@ public final class CallNumberSortFilter extends TokenFilter {
 
 //  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private final CharTermAttribute termAttr = addAttribute(CharTermAttribute.class);
+  private final List<String> prefixes;
 
-  protected CallNumberSortFilter(TokenStream input) {
+  protected CallNumberSortFilter(TokenStream input, List<String> prefixes) {
     super(input);
+    this.prefixes = Collections.unmodifiableList(prefixes);
   }
 
   @Override
@@ -29,27 +33,58 @@ public final class CallNumberSortFilter extends TokenFilter {
 
     if (!input.incrementToken()) return false;
 
-    String outputValue = callNumberSortForm(termAttr);
+    String outputValue = callNumberSortForm(termAttr,prefixes);
     termAttr.setEmpty();
     termAttr.append(outputValue);
     return true;
   }
 
-  public static String callNumberSortForm( CharSequence callNumber ) {
-    return Normalizer.normalize(callNumber, Normalizer.Form.NFD)
-        .toLowerCase()
+  public static String callNumberSortForm( CharSequence callNumber, List<String> prefixes ) {
 
+    String lc = Normalizer.normalize(callNumber, Normalizer.Form.NFD)
+        .toLowerCase().trim()
         // periods not followed by digits aren't decimals and must go
-        .replaceAll("\\.(?!\\d)", " ")
+        .replaceAll("\\.(?!\\d)", " ").replaceAll("\\s+"," ");
+
+    if (lc.isEmpty()) return lc;
+
+    return stripPrefixes(lc,prefixes)
         // all remaining non-alphanumeric (incl decimals) must go
         .replaceAll("[^a-z\\d\\.]+", " ")
         // separate alphabetic and numeric sections
         .replaceAll("([a-z])(\\d)", "$1 $2")
         .replaceAll("(\\d)([a-z])", "$1 $2")
-        // zero pad integer number components (digits not preceded by periods)
-        .replaceAll("(?<![\\.\\d])(\\d+)", "00000000$1")
-        .replaceAll("(?<![\\.\\d])0*(\\d{9})", "$1")
+        // zero pad first integer number component if preceded by
+        // not more than one alphabetic block
+        .replaceAll("^\\s*([a-z]*\\s*)(\\d+)", "$100000000$2")
+        .replaceAll("^([a-z]*\\s*)0*(\\d{9})", "$1$2")
 
         .trim();
+  }
+
+  private static String stripPrefixes(String callnum, List<String> prefixes) {
+
+    if (prefixes == null || prefixes.isEmpty())
+      return callnum;
+
+    String previouscallnum;
+
+    do {
+
+      previouscallnum = callnum;
+      PREF: for (String prefix : prefixes) {
+        if (callnum.startsWith(prefix)) {
+          callnum = callnum.substring(prefix.length());
+          break PREF;
+        }
+      }
+
+      while (callnum.length() > 0 && -1 < " ,;".indexOf(callnum.charAt(0))) {
+        callnum = callnum.substring(1);
+      }
+
+    } while (callnum.length() > 0 && ! callnum.equals(previouscallnum)); 
+
+    return callnum;
   }
 }
--- src/main/java/edu/cornell/library/integration/solr/CallNumberSortFilterFactory.java
+++ src/main/java/edu/cornell/library/integration/solr/CallNumberSortFilterFactory.java
@@ -1,31 +1,89 @@
 package edu.cornell.library.integration.solr;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.util.AbstractAnalysisFactory;
 import org.apache.lucene.analysis.util.MultiTermAwareComponent;
+import org.apache.lucene.analysis.util.ResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoaderAware;
 import org.apache.lucene.analysis.util.TokenFilterFactory;
 
 /**
- * Solr analysis filter for call number sort, which zero pads numbers so they
- * will sort numerically in a string field. Implementation of MultiTermAwareComponent allows
- * filter to work at query time in range queries.
+ * Constructs a {@link CallNumberSortFilter}.
+ * <pre class="prettyprint">
+ * &lt;fieldType name="callNumberSort" class="solr.TextField" sortMissingLast="true" omitNorm="true"&gt;
+ *  &lt;analyzer&gt;
+ *   &lt;tokenizer class="solr.KeywordTokenizerFactory"/&gt;
+ *   &lt;filter class="edu.cornell.library.integration.solr.CallNumberSortFilterFactory" prefixes="callnumberprefixes.txt"/&gt;
+ *  &lt;/analyzer&gt;
+ * &lt;/fieldType&gt;</pre>
+
+ * Solr analysis filter for call number sort, optimized for LC call numbers, but supporting
+ * other call number formats as sort behaviors are identified, to the extent that the call
+ * number formats can be appropriately distinguished from one another.
+ * 
+ * <h2>Call Number Prefixes</h2>
+ * 
+ * Call number prefixes, if provided, will be excluded from sort. Prefixes
+ * typically correspond with MARC holdings 852‡k values, but the filter is only aware of whether the
+ * call number begins with a value identified as a prefix. If not provided, no default prefixes
+ * are assumed, though this could be changed in the future if an option to opt out is also offered.
+ * A prefix file might look like this:
+ * 
+ * <pre>
+ * ## Sample call number prefixes not to sort on ##
+ * Oversize
+ * Microfiche
+ * New Books
+ * Thesis</pre>
+ * 
+ * Note that because lines beginning with hash marks are interpreted as comments, it isn't possible
+ * to include a prefix that itself begins with a hash mark. However, because the sort normalization
+ * already discards the hash mark as irrelevant to sort, the prefix can be included without the hash
+ * mark.<br/><br/>
+ * 
+ * Also, prefixes are compared with each supplied call number in the order they appear in the prefixes
+ * file. So, if call one prefix extends another, the longer prefix should appear first. For example,
+ * listing {@code Rare Books} before {@code Rare} can ensure that the longer prefix will match first,
+ * where applicable.
  *
  */
-public class CallNumberSortFilterFactory extends TokenFilterFactory implements MultiTermAwareComponent {
+public class CallNumberSortFilterFactory 
+  extends TokenFilterFactory implements MultiTermAwareComponent, ResourceLoaderAware {
 
-  public CallNumberSortFilterFactory(Map<String,String> map) {
-    super(map);
+  private List<String> prefixes;
+  private final String prefixFile;
+
+  public CallNumberSortFilterFactory(Map<String,String> args) {
+    super(args);
+    prefixFile = get(args, "prefixes");
   }
 
   @Override
   public TokenStream create(TokenStream input) {
-    return new CallNumberSortFilter(input);
+    return new CallNumberSortFilter(input, prefixes);
   }
 
   @Override
   public AbstractAnalysisFactory getMultiTermComponent() {
     return this;
+  }
+
+  @Override
+  public void inform(ResourceLoader loader) throws IOException {
+    if (prefixFile != null) {
+      List<String> temp = getLines(loader, prefixFile);
+      prefixes = new ArrayList<>();
+      for (String s : temp)
+        prefixes.add(s.toLowerCase().replaceAll("\\.(?!\\d) *"," "));
+    }
+  }
+
+  public List<String> getPrefixes() {
+    return prefixes;
   }
 }
