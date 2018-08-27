c6c7ff8c3f94653c472829c9d39463244d933f17
Frances Webb <frances.webb@cornell.edu>
2018-08-27 13:41:31 -0400

    Add script to refresh Solr documents' availability using Solr timestamps
    This will re-index records in Solr in the main blacklight and call number indexes, based on the oldest Solr timestamps in the blacklight index. Re-indexed records have updated availability, but main record changes aren't applied.

--- src/main/java/edu/cornell/library/integration/availability/RecordsToSolr.java
+++ src/main/java/edu/cornell/library/integration/availability/RecordsToSolr.java
@@ -88,7 +88,7 @@ public class RecordsToSolr {
       }
       return sb.toString();
     }
-    public enum Type { BIB, HOLDING, ITEM, CIRC, RECEIPT, OTHER };
+    public enum Type { BIB, HOLDING, ITEM, CIRC, RECEIPT, AGE, OTHER };
     private static DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT,FormatStyle.MEDIUM);
 
     @Override
