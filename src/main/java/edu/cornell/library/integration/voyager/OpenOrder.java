46ce7135d03b0ed6127664a2efffc7102f2c92c3
Frances Webb <frances.webb@cornell.edu>
2019-04-17 16:44:28 -0400

    Add order detection for pending orders
    This implements the second bullet point in DISCOVERYACCESS-4906.
    
    Pending orders will be displayed as "In pre-order processing as of [date]", using the line item copy status date, or simply "In pre-order processing" when the line item copy status is undated.
    
    Also, the display of order dates has been switched to the m/d/yy format that is the default for unavailable item status and due date displays, and the unit tests for OpenOrder are now exclusively using the canned test sqlite database rather than the live catalog database.

--- src/main/java/edu/cornell/library/integration/voyager/OpenOrder.java
+++ src/main/java/edu/cornell/library/integration/voyager/OpenOrder.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 class OpenOrder {
 
@@ -15,11 +17,12 @@ class OpenOrder {
         "SELECT line_item.quantity,"+
         "       purchase_order.po_approve_date,"+
         "       line_item_copy_status.mfhd_id,"+
-        "       line_item_copy_status.line_item_status"+
+        "       line_item_copy_status.line_item_status,"+
+        "       line_item_copy_status.status_date"+
         "  FROM line_item, line_item_copy_status, purchase_order"+
         " WHERE line_item.bib_id = ?"+
         "   AND line_item.line_item_id = line_item_copy_status.line_item_id"+
-        "   AND line_item_copy_status.line_item_status IN (7, 8)"+ // i.e. (cancelled, approved), neither received
+        "   AND line_item_copy_status.line_item_status IN (0, 7, 8)"+ // i.e. (pending, cancelled, approved), not received
         "   AND line_item.po_id = purchase_order.po_id"+
         "   AND purchase_order.po_type = 1"; // firm order (not ongoing)
     try (PreparedStatement pstmt = voyager.prepareStatement(getOrderInfoQuery)) {
@@ -27,14 +30,23 @@ class OpenOrder {
       try (ResultSet rs = pstmt.executeQuery()) {
         while (rs.next()) {
           switch (rs.getInt("line_item_status")) {
+          case 0:
+            this.note = "In pre-order processing";
+            Timestamp statusDate = rs.getTimestamp("status_date");
+            if ( statusDate != null )
+              note += " as of "+format.format(statusDate); break;
           case 7:
             this.note = "Order cancelled"; break;
           case 8:
-            this.note = rs.getInt("quantity")+" Copy Ordered as of "+rs.getString("po_approve_date").substring(0,10);
+            int quantity = rs.getInt("quantity");
+            this.note = String.format("%d cop%s ordered as of %s",
+                quantity,(quantity==1)?"y":"ies",format.format(rs.getTimestamp("po_approve_date")));
           }
           this.mfhdId = rs.getInt("mfhd_id");
         }
       }
     }
   }
+
+  private SimpleDateFormat format = new SimpleDateFormat("M/d/yy");
 }
--- src/test/java/edu/cornell/library/integration/voyager/OpenOrderTest.java
+++ src/test/java/edu/cornell/library/integration/voyager/OpenOrderTest.java
@@ -22,7 +22,7 @@ import edu.cornell.library.integration.voyager.OpenOrder;
 public class OpenOrderTest {
 
   static Connection voyagerTest = null;
-  static Connection voyagerLive = null;
+//  static Connection voyagerLive = null;
 
   @BeforeClass
   public static void connect() throws SQLException, ClassNotFoundException, IOException {
@@ -32,32 +32,52 @@ public class OpenOrderTest {
     }
     Class.forName("org.sqlite.JDBC");
     voyagerTest = DriverManager.getConnection("jdbc:sqlite:src/test/resources/voyagerTest.db");
-    Class.forName("oracle.jdbc.driver.OracleDriver");
-    voyagerLive = DriverManager.getConnection(
-        prop.getProperty("voyagerDBUrl"),prop.getProperty("voyagerDBUser"),prop.getProperty("voyagerDBPass"));
+//    Class.forName("oracle.jdbc.driver.OracleDriver");
+//    voyagerLive = DriverManager.getConnection(
+//        prop.getProperty("voyagerDBUrl"),prop.getProperty("voyagerDBUser"),prop.getProperty("voyagerDBPass"));
+  }
+
+
+  @Test
+  public void onOrder() throws SQLException {
+    OpenOrder o = new OpenOrder(voyagerTest, 10797688);
+    assertEquals( "1 copy ordered as of 4/17/19", o.note );
   }
 
   @Test
-  public void onOrderTest() throws SQLException, IOException, XMLStreamException {
+  public void orderPreprocessing() throws SQLException {
+
+    // Pending order, show pre-order processing, and line item copy status date
+    OpenOrder o = new OpenOrder(voyagerTest, 10797795);
+    assertEquals( "In pre-order processing as of 4/17/19", o.note );
+
+    // Pending order, show pre-order processing, no available line item copy status date
+    o = new OpenOrder(voyagerTest, 10797341);
+    assertEquals( "In pre-order processing", o.note );
+
+  }
+
+  @Test
+  public void availableForPurchaseWithCancelledOrder() throws SQLException, IOException, XMLStreamException {
 
     int bibId = 9386182;
 
     // First, we can find the order information independently
-    OpenOrder o = new OpenOrder(voyagerLive, bibId);
+    OpenOrder o = new OpenOrder(voyagerTest, bibId);
     assertEquals( "Order cancelled", o.note );
 
     // Second, we don't pull it into holdings because of the call number
-    HoldingSet holdings = Holdings.retrieveHoldingsByBibId(voyagerLive, bibId);
-    ItemList items = Items.retrieveItemsForHoldings(voyagerLive, null, bibId, holdings);
+    HoldingSet holdings = Holdings.retrieveHoldingsByBibId(voyagerTest, bibId);
+    ItemList items = Items.retrieveItemsForHoldings(voyagerTest, null, bibId, holdings);
     holdings.summarizeItemAvailability(items);
-    holdings.applyOpenOrderInformation(voyagerLive, bibId);
+    holdings.applyOpenOrderInformation(voyagerTest, bibId);
     assertEquals( "Available for the Library to Purchase", holdings.get(9720246).call );
     assertNull( holdings.get(9720246).orderNote );
 
     // But without that call number, the lack of items will allow the order note
     holdings.get(9720246).call = "ABC123";
-    holdings.applyOpenOrderInformation(voyagerLive, bibId);
+    holdings.applyOpenOrderInformation(voyagerTest, bibId);
     assertEquals( "Order cancelled", holdings.get(9720246).orderNote );
-  }
 
+  }
 }
