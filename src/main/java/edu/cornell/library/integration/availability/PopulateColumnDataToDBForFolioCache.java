9b46a923b2acda0f55d636b328f817635c1a3912
Frances Webb <frances.webb@cornell.edu>
2021-08-10 12:49:21 -0400

    Repurpose one-off data mediation tool

--- src/main/java/edu/cornell/library/integration/availability/PopulateColumnDataToDBForFolioCache.java
+++ src/main/java/edu/cornell/library/integration/availability/PopulateColumnDataToDBForFolioCache.java
@@ -12,7 +12,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /** One-off data remediation method */
-public class PopulateBarcodesToDBForPreLoadItems {
+public class PopulateColumnDataToDBForFolioCache {
 
   public static void main(String[] args) throws IOException, SQLException {
     Properties prop = new Properties();
@@ -26,33 +26,32 @@ public class PopulateBarcodesToDBForPreLoadItems {
         prop.getProperty("inventoryDBUser"),
         prop.getProperty("inventoryDBPass"));
         PreparedStatement q = inventory.prepareStatement(
-            "SELECT * FROM itemFolio"
-            + " WHERE barcode IS NULL AND content LIKE '%\"barcode\"%' LIMIT 100");
+            "SELECT * FROM instanceFolio WHERE source IS NULL LIMIT 1000");
         PreparedStatement u = inventory.prepareStatement(
-            "UPDATE itemFolio SET barcode = ? WHERE id = ?")) {
+            "UPDATE instanceFolio SET source = ? WHERE id = ?")) {
       int foundCount;
       do {
         foundCount = 0;
         try ( ResultSet rs = q.executeQuery() ) {
           while ( rs.next() ) {
             foundCount++;
-            Matcher m = barcodeP.matcher(rs.getString("content"));
+            Matcher m = sourceP.matcher(rs.getString("content"));
             if ( ! m.matches() ) {
-              System.out.println("No barcode: "+rs.getString("content"));
+              System.out.println("No source: "+rs.getString("content"));
               continue;
             }
-            String barcode = m.group(1);
-            if (barcode.length() > 15) continue;
-            u.setString(1, barcode);
+            String source = m.group(1);
+            if (source.length() > 12) continue;
+            u.setString(1, source);
             u.setString(2, rs.getString("id"));
             u.addBatch();
           }
           u.executeBatch();
         }
-      } while ( foundCount == 100 );
+      } while ( foundCount == 1000 );
       
     }
   }
-  static Pattern barcodeP = Pattern.compile("^.*\"barcode\": *\"([^\"]+)\".*$");
+  static Pattern sourceP = Pattern.compile("^.*\"source\": *\"([^\"]+)\".*$");
 
 }
