a2195d3976600342c9c14664e0e534fa8a8329be
Frances Webb <frances.webb@cornell.edu>
2021-09-20 14:34:33 -0400

    Skip null elements on holdingsStatementsForIndexes list in holding

--- src/main/java/edu/cornell/library/integration/folio/Holding.java
+++ src/main/java/edu/cornell/library/integration/folio/Holding.java
@@ -183,6 +183,7 @@ public class Holding {
       List<Map<String,String>> a = (List<Map<String,String>>)raw.get("holdingsStatementsForIndexes");
       List<String> holdings = new ArrayList<>();
       for (Map<String,String> statement : a) {
+        if (statement == null ) continue;
         List<String> parts = new ArrayList<>();
         if ( statement.containsKey("statement") ) parts.add(statement.get("statement"));
         if ( statement.containsKey("note") )      parts.add(statement.get("note"));
