dd48b9d0c798e92725d7a673015d257c56061e39
Frances Webb <frances.webb@cornell.edu>
2024-11-13 14:00:23 -0500

    Try chunking export files by record count not size
    DACCESS-397

--- src/main/java/edu/cornell/library/integration/exports/CC0Export.java
+++ src/main/java/edu/cornell/library/integration/exports/CC0Export.java
@@ -44,7 +44,9 @@ public class CC0Export {
       Set<String> bibs = ExportUtils.getBibsToExport(inventory);
       System.out.println("Bib count: "+bibs.size());
       int fileCount = 0;
-      String filenamePattern = "cornell-export-200mb-chunks-%03d.xml";
+      String filenamePattern = "cornell-export-300k-rec-chunks-%02d.xml";
+      int recordsPerFile = 300_000;
+      int recordsThisFile = 0;
       Path outputFile = Paths.get(String.format(filenamePattern,++fileCount));
       BufferedWriter writer = Files.newBufferedWriter(outputFile);
       writer.write("<?xml version='1.0' encoding='UTF-8'?>"
@@ -79,7 +81,8 @@ public class CC0Export {
         ExportUtils.cleanUnwantedDataFields(bibRec, null, Arrays.asList(new ExportUtils.FieldRange("857","999")),false);
         writer.write(bibRec.toString("xml").replace("<?xml version='1.0' encoding='UTF-8'?>", "")
             .replace(" xmlns=\"http://www.loc.gov/MARC21/slim\"","")+"\n");
-        if ( Files.size(outputFile) > 209715200L /*200MB*/ ) {
+        if ( ++recordsThisFile == recordsPerFile ) {
+//        if ( Files.size(outputFile) > 209715200L /*200MB*/ ) {
           writer.write("</collection>\n");
           writer.flush();
           writer.close();
@@ -87,6 +90,7 @@ public class CC0Export {
           writer = Files.newBufferedWriter(outputFile);
           writer.write("<?xml version='1.0' encoding='UTF-8'?>"
               + "<collection xmlns=\"http://www.loc.gov/MARC21/slim\">\n");
+          recordsThisFile = 0;
         }
       }
       writer.write("</collection>\n");
