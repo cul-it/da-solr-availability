d012d8a9800d6a353d329a1a9367365beb78a9f5
Frances Webb <frances.webb@cornell.edu>
2025-08-20 14:58:48 -0400

    Use configured properties file

--- src/main/java/edu/cornell/library/integration/folio/MonitorFolioChanges.java
+++ src/main/java/edu/cornell/library/integration/folio/MonitorFolioChanges.java
@@ -1,5 +1,7 @@
 package edu.cornell.library.integration.folio;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -25,10 +27,16 @@ public class MonitorFolioChanges {
 
   public static void main(String[] args) throws IOException, SQLException, InterruptedException, AuthenticationException {
 
+    Map<String, String> env = System.getenv();
+    String configFile = env.get("configFile");
+    if (configFile == null)
+      throw new IllegalArgumentException("configFile must be set in environment to valid file path.");
     Properties prop = new Properties();
-    try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties")){
-      prop.load(in);
-    }
+    File f = new File(configFile);
+    if (f.exists()) {
+      try ( InputStream is = new FileInputStream(f) ) { prop.load( is ); }
+    } else System.out.println("File does not exist: "+configFile);
+
 
     try (
         Connection inventory = DriverManager.getConnection(
